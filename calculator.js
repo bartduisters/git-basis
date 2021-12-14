c39654cf84839dcd596c3663f61a3f996c95b3ac
Bart Duisters <bartduisters@bartduisters.com>
2021-12-14 19:35:46 +0100

    feat: subtract in calculator

--- calculator.js
+++ calculator.js
@@ -1,11 +1,13 @@
 const calculator = () => {
   const sum = (a, b) => a + b;
   const multiply = (a, b) => a * b; 
+  const subtract = (a, b) => a - b;
 
-  return { sum, multiply };
+  return { sum, multiply, subtract };
 };
 
-const { sum, multiply } = calculator();
+const { sum, multiply, subtract } = calculator();
 
 console.log(sum(2, 3));
 console.log(multiply(2, 3));
+console.log(subtract(2, 3));
